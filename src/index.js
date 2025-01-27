48559432cb3bb3d66b4a65c9c938d66161d1d03a
MarkusWaller <waller.markus@gmail.com>
2025-01-27 20:04:01 +0000

    Auth und User hinzugefügt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const basicAuth = require('basic-auth');
+
+const auth = function (req, res, next) {
+  const user = basicAuth(req);
+  if (!user || !user.name || !user.pass) {
+    res.set('WWW-Authenticate', 'Basic realm="example"');
+    return res.status(401).send('Authentication required.');
+  }
+
+  if (user.name === 'admin' && user.pass === 'password') {
+    return next();
+  } else {
+    res.set('WWW-Authenticate', 'Basic realm="example"');
+    return res.status(401).send('Authentication required.');
+  }
+}
+
 const express = require('express');
 require('dotenv').config();
 
@@ -7,9 +24,23 @@ let users = [
   { id: 1, name: 'Alice' },
   { id: 2, name: 'Bob' }
 ];
+
+
 // Default-Route
-app.get('/', (req, res) => {
+app.get('/', auth, (req, res) => {
   res.send('Willkommen auf der API! Ergänzen Sie die URL um "/api/users" für den Endpunkt.');
+});
+
+app.post('/users', (req, res) => {
+  const newUser = {
+    id: users.length + 1, 
+    name: req.body.name,
+    // ... weitere User-Daten ... 
+  };
+
+  users.push(newUser);
+
+  res.status(201).json(newUser);
 });
 
 // Beispielroute
